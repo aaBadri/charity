31eac2847068c404fcdb423851e6e92e5fd7a403
aaBadri <aliakbar.badr@gmail.com>
2024-06-02 06:47:03 +0330

    add account creation

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -4,4 +4,35 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    pass
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        # help_text="Leave empty if no change needed",
+        # style={"input_type": "password", "placeholder": "Password"},
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "password",
+            "phone",
+            "address",
+            "gender",
+            "age",
+            "description",
+            "first_name",
+            "last_name",
+            "email",
+        )
+
+    def create(self, validated_data):
+        # user = User.objects.create_user(
+        #     username=validated_data["username"],
+        #     password=validated_data["password"],
+        # )
+        user = User(**validated_data)
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
+
